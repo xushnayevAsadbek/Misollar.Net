3557709d29abd294f39d0a362674531d5ebc8fd0
Asadbek Xushnayev <xushnaevasadbek@gmail.com>
2025-08-25 10:32:11 +0500

    if oid misollar

--- Program.cs
+++ Program.cs
@@ -19,6 +19,63 @@ else{
 Console.WriteLine("Qiymat: " + Qiymat);
 */
 
-// 2-misol
+// 2-misol 3-misol
 
+/*
+ * int son = 16;
+Console.WriteLine(son % 3); 
+*/
+
+// 4-misol 
+
+/*
+Console.Write("Sonni kiriting:");
+int son= int.Parse(Console.ReadLine());
+if (son % 2 == 0)
+{
+    Console.WriteLine("Juft son");
+}
+else
+{
+    Console.WriteLine("Toq son");
+}
+*/
+
+//5-misol 
+/*
+ Console.Write("a sonni kiriting:");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b sonni kiriting:");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("a soni katta");
+}
+else if (a < b)
+{
+    Console.WriteLine("b soni katta");
+}
+else
+{
+    Console.WriteLine("Sonlar teng");
+}
+*/
+
+// 6-misol Son yasash
+/*
+Console.Write("a sonni kiriting:");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b sonni kiriting:");
+int b = int.Parse(Console.ReadLine()); 
+Console.Write("c sonni kiriting:");
+int c = int.Parse(Console.ReadLine());
+if(a>0 && b>0 && c > 0)
+{
+    Console.WriteLine(a*100 +   b*10 + c);
+}
+else
+{
+    Console.WriteLine(0);
+}
+*/
 
