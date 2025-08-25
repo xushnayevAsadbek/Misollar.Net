9f313d5c0607251cb6ad8bb5ebfcb424a2f2f0ac
Asadbek Xushnayev <xushnaevasadbek@gmail.com>
2025-08-25 10:03:31 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// 1-misol 
+/* Console.Write("Son kiriting:");
+int son = int.Parse(Console.ReadLine());
+int Qiymat;
+if (son > 10)
+{
+    Qiymat = son + 3;
+}
+else if (son < 10){
+    Qiymat = son * 2;
+}
+else{
+    Qiymat = 22;
+}
+
+Console.WriteLine("Qiymat: " + Qiymat);
+*/
+
+// 2-misol
+
+
